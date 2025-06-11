7e3abd9cdb72b16b2b2db8880c11ca9c5d1ced1b
Ehab Ahmed <ahmed.ehabj@gmail.com>
2025-06-11 21:51:10 +0400

    Added Model Domains for all three tables - Walk, Region and Difficulty.

--- NZWalks.API/Models/Domain/Walk.cs
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -2,6 +2,7 @@
 {
     public class Walk
     {
+        // DB Table Schema
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
